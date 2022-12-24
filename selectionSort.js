b6eb3292b9488810125bae8107d75f5d661f7d1c
Victor Chai <Vic.P.Chai@gmail.com>
2022-12-24 15:13:10 -0500

    selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,12 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+  }
+  return arr;
+}
